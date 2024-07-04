e8e0dc7d1f30469beeb9cf2650a70c19cdbaf27b
devdimer <henrique.mdimer@gmail.com>
2024-07-04 16:05:22 -0300

    improve configuration

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 vim.g.mapleader = "<Space>"
 vim.cmd('set shiftwidth=2')
+vim.cmd('set number')
+vim.cmd [[
+  set tabstop=2
+  set softtabstop=2
+  set shiftwidth=2
+  set expandtab
+]]
 
 require("config.lazy")
 require("config.lspconfig")
@@ -7,4 +14,10 @@ require("config.mappings")
 require("config.whichkey")
 require("config.statusline")
 
-vim.cmd[[colorscheme dracula]]
+vim.cmd [[colorscheme dracula]]
+vim.cmd('set wrap!')
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    vim.lsp.buf.format()
+  end
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,4 +99,22 @@ return {
       require('nvim-autopairs').setup()
     end
   },
+  {
+    "Zeioth/compiler.nvim",
+    cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+  },
+  {
+    "stevearc/overseer.nvim",
+    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+	min_height = 25,
+	max_height = 25,
+	default_detail = 1
+      },
+    },
+  },
 }
