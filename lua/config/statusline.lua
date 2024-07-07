6a25db02b3d60944a10404a4e85ae77b1194b90a
devdimer <henrique.mdimer@gmail.com>
2024-07-07 16:02:17 -0300

    add plugins & configure

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -3,34 +3,6 @@
 -- MIT license, see LICENSE for more details.
 -- stylua: ignore
 
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.white },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
-  },
-}
-
 require('lualine').setup {
   options = {
     theme = 'dracula-nvim',
@@ -43,7 +15,7 @@ require('lualine').setup {
     lualine_c = {
       '%=', --[[ add your center compoentnts here in place of this comment ]]
     },
-    lualine_x = {'diagnostics'},
+    lualine_x = { 'diagnostics' },
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
       { 'location', separator = { right = '' }, left_padding = 2 },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,67 +31,7 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = { "onsails/lspkind.nvim" },
     config = function()
-      vim.cmd('set completeopt=menu,menuone,noselect')
-
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' }
-        }, {
-          { name = 'buffer' }
-        }),
-        window = {
-          completion = {
-            border = 'rounded',
-            scrollbar = '||',
-          },
-          documentation = {
-            border = 'rounded',
-            scrollbar = '||'
-          },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expandable() then
-              luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              -- cmp.complete()
-              fallback()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-            return kind
-          end,
-        },
-      })
+      require('config.cmp')
     end
   },
   {
@@ -106,7 +46,6 @@ return {
     "Zeioth/compiler.nvim",
     cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
     dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
-    opts = {},
   },
   {
     event = "VeryLazy",
@@ -120,5 +59,32 @@ return {
         default_detail = 1
       },
     },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require('config.dap')
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require('dap')
+      local ui = require('dapui')
+
+      ui.setup()
+
+      dap.listeners.before.event_initialized['dapui_config'] = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        ui.close()
+      end
+    end
   },
 }
