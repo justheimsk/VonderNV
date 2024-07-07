a06b0332255eec493b5c7936972d66b01ddb869d
devdimer <henrique.mdimer@gmail.com>
2024-07-07 16:49:17 -0300

    add support for css/scss completion

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -7,3 +7,20 @@ lspconfig.clangd.setup({})
 lspconfig.biome.setup({})
 lspconfig.jsonls.setup({})
 lspconfig.emmet_language_server.setup({})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits"
+    }
+  }
+}
+
+lspconfig.cssls.setup({
+  capabilities = capabilities
+})
+lspconfig.css_variables.setup({})
